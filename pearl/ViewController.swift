74c73bf4441e592a478ccca9d28462a482d20fc9
maskjun <mask.jun@gmail.com>
2024-10-21 10:42:28 +0800

    init

---
+++ pearl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pearl
+//
+//  Created by 马军 on 2024/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pearl/ViewModel/homeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  homeViewModel.swift
+//  pearl
+//
+//  Created by 马军 on 2024/10/21.
+//
+
+import Foundation
